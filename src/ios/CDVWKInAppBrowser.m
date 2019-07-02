013f56276a69d5cfbe5cdda1a9c06de9cedc50ef
Rouven Retzlaff (BERIS) <rouven.retzlaff@Rouvens-MacBook-Pro.local>
2019-07-02 17:45:40 +0200

    Mailto links can now be opened in mail app (ios)

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -554,7 +554,7 @@ static CDVWKInAppBrowser* instance = nil;
     }
     
     //if is an app store link, let the system handle it, otherwise it fails to load it
-    if ([[ url scheme] isEqualToString:@"itms-appss"] || [[ url scheme] isEqualToString:@"itms-apps"]  || [[ url scheme] isEqualToString:@"whatsapp"]) {
+    if ([[ url scheme] isEqualToString:@"itms-appss"] || [[ url scheme] isEqualToString:@"itms-apps"]  || [[ url scheme] isEqualToString:@"whatsapp"]  || [[ url scheme] isEqualToString:@"mailto"]) {
         [theWebView stopLoading];
         [self openInSystem:url];
         shouldStart = NO;
